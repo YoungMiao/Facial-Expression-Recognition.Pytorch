34dc503d32bb6b106ed3408c2a195dbe41816225
maple <yangm9875@126.com>
2019-04-02 10:22:38 +0800

    add mtcnn

---
+++ cameraDemo.py
@@ -0,0 +1,143 @@
+"""
+visualize results for test image
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+from torch.autograd import Variable
+import cv2
+
+import transforms as transforms
+from skimage import io
+from skimage.transform import resize
+from models import *
+
+from src import detect_faces
+from PIL import Image
+
+cut_size = 44
+
+transform_test = transforms.Compose([
+    transforms.TenCrop(cut_size),
+    transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+])
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+# raw_img = io.imread('images/1.jpg')
+# gray = rgb2gray(raw_img)
+# gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
+
+# img = gray[:, :, np.newaxis]
+
+# img = np.concatenate((img, img, img), axis=2)
+# img = Image.fromarray(img)
+# inputs = transform_test(img)
+
+class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+net = VGG('VGG19')
+checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'))
+net.load_state_dict(checkpoint['net'])
+net.cuda('gpu:0')
+net.eval()
+
+margin = 40
+
+cam = cv2.VideoCapture(0)
+while True:
+    ret,frame = cam.read()
+    PILimage = Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+    bounding_boxes, landmarks = detect_faces(PILimage)
+
+    #print(bounding_boxes[:])
+    for bbox in bounding_boxes:
+        det = np.squeeze(bbox[:4])
+        
+        # face rectangle
+        det[0] = np.maximum(det[0] - margin, 0)
+        det[1] = np.maximum(det[1] - margin, 0)
+        det[2] = np.minimum(det[2] + margin, frame.shape[1])
+        det[3] = np.minimum(det[3] + margin, frame.shape[0])
+        
+        # face cropped
+        bb = np.array(det, dtype=np.int32)
+        frame_copy = frame.copy()
+        cropped = frame_copy[bb[1]:bb[3], bb[0]:bb[2], :]
+        
+        
+        gray = rgb2gray(cropped)
+        gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
+
+        img = gray[:, :, np.newaxis]
+
+        img = np.concatenate((img, img, img), axis=2)
+        img = Image.fromarray(img)
+        inputs = transform_test(img)
+
+        ncrops, c, h, w = np.shape(inputs)
+
+        inputs = inputs.view(-1, c, h, w)
+        inputs = inputs.cuda()
+        inputs = Variable(inputs, volatile=True)
+        outputs = net(inputs)
+
+        outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
+
+        score = F.softmax(outputs_avg)
+        _, predicted = torch.max(outputs_avg.data, 0)
+        print(class_names[int(predicted.cpu().numpy())])
+        cv2.rectangle(frame, (bb[0],bb[1]),(bb[2],bb[3]), (0,255,0), 5)
+        cv2.putText(frame, 'emotion : %s' % (class_names[int(predicted.cpu().numpy())]), (bb[0] - 10, bb[1] - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.75,
+                                (0, 255, 0), 2)
+    cv2.imshow('demo',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cam.release()
+cv2.destoryAllWindows()
+# plt.rcParams['figure.figsize'] = (13.5,5.5)
+# axes=plt.subplot(1, 3, 1)
+# plt.imshow(raw_img)
+# plt.xlabel('Input Image', fontsize=16)
+# axes.set_xticks([])
+# axes.set_yticks([])
+# plt.tight_layout()
+
+
+# plt.subplots_adjust(left=0.05, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
+
+# plt.subplot(1, 3, 2)
+# ind = 0.1+0.6*np.arange(len(class_names))    # the x locations for the groups
+# width = 0.4       # the width of the bars: can also be len(x) sequence
+# color_list = ['red','orangered','darkorange','limegreen','darkgreen','royalblue','navy']
+# for i in range(len(class_names)):
+    # plt.bar(ind[i], score.data.cpu().numpy()[i], width, color=color_list[i])
+# plt.title("Classification results ",fontsize=20)
+# plt.xlabel(" Expression Category ",fontsize=16)
+# plt.ylabel(" Classification Score ",fontsize=16)
+# plt.xticks(ind, class_names, rotation=45, fontsize=14)
+
+# axes=plt.subplot(1, 3, 3)
+# emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
+# plt.imshow(emojis_img)
+# plt.xlabel('Emoji Expression', fontsize=16)
+# axes.set_xticks([])
+# axes.set_yticks([])
+# plt.tight_layout()
+# show emojis
+
+# plt.show()
+# plt.savefig(os.path.join('images/results/l.png'))
+# plt.close()
+
+# print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
+
+
